980e988f54340d8b5626e99493ee24ab0f540bdc
CarinWood <carin.wood.85@gmail.com>
2024-02-09 16:09:29 +0100

    created function searchBeer

--- js/search.js
+++ js/search.js
@@ -1,12 +1,22 @@
 let inputField = document.querySelector(".input-field");
 
+async function searchBeer(beerName) {
+  try {
+    const res = await fetch(
+      `https://api.punkapi.com/v2/beers?beer_name=${beerName}`
+    );
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    const searchForm = document.getElementById('searchForm');
-  
-    searchForm.addEventListener('submit', function(e) {
-      e.preventDefault(); 
-      console.log(inputField.value);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const searchForm = document.getElementById("searchForm");
+
+  searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    searchBeer(inputField.value);
   });
-  
+});
