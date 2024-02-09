b58bf51cbb63137a146ccc51948f50668392affa
CarinWood <carin.wood.85@gmail.com>
2024-02-09 16:41:38 +0100

    fix with search results

--- js/search.js
+++ js/search.js
@@ -1,12 +1,17 @@
 let inputField = document.querySelector(".input-field");
 
 async function searchBeer(beerName) {
+  document.querySelector(".search-results").innerHTML = "";
   try {
     const res = await fetch(
-      `https://api.punkapi.com/v2/beers?beer_name=${beerName}`
+      `https://api.punkapi.com/v2/beers?beer_name=${beerName}&page=1&per_page=10`
     );
     const data = await res.json();
-    console.log(data);
+    data.forEach((beer) => {
+      let beerResult = document.createElement("p");
+      beerResult.innerText = beer.name;
+      document.querySelector(".search-results").appendChild(beerResult);
+    });
   } catch (error) {
     console.error("Error", error);
   }
